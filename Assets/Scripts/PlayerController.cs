32a18ca4f59f39a29539ab28c4e28ce5f6a568d8
alya1007 <alexandra.konjevic@isa.utm.md>
2023-05-17 16:55:23 +0300

    fixed movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f; // Adjust this to control the player's movement speed
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal"); // Get input from horizontal axis (e.g., A and D keys or left and right arrows)
+
+        Vector2 movement = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+
+        // flip when walking right
+        if (horizontalInput < 0)
+        {
+            transform.localScale = new Vector3(2, 2, 2);
+        }
+        // flip when walking left
+        else if (horizontalInput > 0)
+        {
+            transform.localScale = new Vector3(-2, 2, 2);
+        }
+        rb.velocity = movement;
+    }
+}
