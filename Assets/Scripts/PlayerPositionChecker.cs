d2a86b3ece2e4c17b88d559926e0aa3eb075f4c0
alya1007 <alexandra.konjevic@isa.utm.md>
2023-05-18 16:27:26 +0300

    enter button handle

---
+++ Assets/Scripts/PlayerPositionChecker.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerPositionChecker : MonoBehaviour
+{
+    public string targetTag = "Player";
+    public float minX = -9.4f;
+    public float maxX = -5.28f;
+    public Canvas canvasToToggle;
+    public int sceneToLoadIndex = 2;
+    private bool isInRange = false;
+
+    private void Update()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag(targetTag);
+        if (player != null)
+        {
+            float playerX = player.transform.position.x;
+            if (playerX >= minX && playerX <= maxX)
+            {
+                isInRange = true;
+                Debug.Log(isInRange);
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    SceneManager.LoadScene(sceneToLoadIndex);
+                }
+                canvasToToggle.gameObject.SetActive(true);
+            }
+            else
+            {
+                isInRange = false;
+                canvasToToggle.gameObject.SetActive(false);
+            }
+        }
+    }
+}
