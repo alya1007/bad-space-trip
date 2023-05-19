547d192479067b3d969352d0b2a97d003e841ae3
Sevastian Bajenov <sevabaj@gmail.com>
2023-05-19 08:29:51 +0300

    Extend box collider for the wolf's movement.

--- Assets/Scripts/AnimalController.cs
+++ Assets/Scripts/AnimalController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class AnimalController : MonoBehaviour
 {
+    public float moveSpeed = 4f;
+
+    public float attackRange = 8f;
     // Start is called before the first frame update
     void Start()
     {
