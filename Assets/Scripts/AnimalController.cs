2b095cadd85f06349927344960992f8c6b63dd78
Sevastian Bajenov <sevabaj@gmail.com>
2023-05-19 09:22:41 +0300

    Implement npc movement.

--- Assets/Scripts/AnimalController.cs
+++ Assets/Scripts/AnimalController.cs
@@ -4,18 +4,48 @@ using UnityEngine;
 
 public class AnimalController : MonoBehaviour
 {
-    public float moveSpeed = 4f;
+    public GameObject player;
 
-    public float attackRange = 8f;
-    // Start is called before the first frame update
+    public float moveSpeed = 6f;
+    public float followRange = 15f;
+    public float attackRange = 3f;
+
+    public Vector3 spawnPoint = new Vector3(-14.76f, -2.64f, 0);
+
+    private Rigidbody2D animalRigidBody;
+    
     void Start()
     {
-        
+        animalRigidBody = GetComponent<Rigidbody2D>();
+        transform.position = spawnPoint;   
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+        if (distanceToPlayer <= followRange)
+        {
+            FollowPlayer();
+
+            if (distanceToPlayer <= attackRange)
+            {
+                Attack();
+            }
+        }
+    }
+
+    void FollowPlayer()
+    {
+        Vector3 direction = (player.transform.position - transform.position).normalized;
+        // transform.localScale = new Vector3(-1, 1, 0);
+        GetComponent<SpriteRenderer>().flipX = true;
+        animalRigidBody.velocity = direction * moveSpeed;
+    }
+
+    void Attack()
+    {
+        // Implement attack logic
+        // ...
     }
 }
